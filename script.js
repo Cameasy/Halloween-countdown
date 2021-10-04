d47c0b93f9ae88f7360ce4287bee304bdb11c614
Yaks Souza <pl4nk1ton@gmail.com>
2021-10-04 07:47:38 -0300

    Using current year stead of 2021.
    - Set halloween date to halloween date of current year stead of 2021 year.
    - To organize the code for more readable.

--- script.js
+++ script.js
@@ -3,26 +3,27 @@ const hoursEl = document.getElementById("hours");
 const minsEl = document.getElementById("mins");
 const secondsEl = document.getElementById("seconds");
 
-const halloween = "31 Oct 2021";
+const halloween = `31 Oct ${new Date().getFullYear()}`;
 
 function countdown(){
-const halloweenDate = new Date(halloween);
-const currentDate = new Date()
+    const halloweenDate = new Date(halloween);
+    const currentDate = new Date();
 
-const totalSeconds = (halloweenDate - currentDate) / 1000;
-let days = Math.floor(totalSeconds / 3600 / 24) ;
-let hours = Math.floor(totalSeconds / 3600) %24;
-let mins = Math.floor(totalSeconds / 60) % 60;
-let seconds = Math.floor(totalSeconds) % 60;
+    const totalSeconds = (halloweenDate - currentDate) / 1000;
+    let days = Math.floor(totalSeconds / 3600 / 24);
+    let hours = Math.floor(totalSeconds / 3600) %24;
+    let mins = Math.floor(totalSeconds / 60) % 60;
+    let seconds = Math.floor(totalSeconds) % 60;
 
-daysEl.innerHTML= days;
-hoursEl.innerHTML = formatTime(hours);
-minsEl.innerHTML = formatTime(mins);
-secondsEl.innerHTML = formatTime(seconds);
+    daysEl.innerHTML= days;
+    hoursEl.innerHTML = formatTime(hours);
+    minsEl.innerHTML = formatTime(mins);
+    secondsEl.innerHTML = formatTime(seconds);
 }
+
 function formatTime(time){
     return time < 10 ? `0${time}` : time;
 }
-countdown();
 
+countdown();
 setInterval(countdown, 1000);
